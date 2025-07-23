8795c9d837c3c443cd91440dfe9f1f7cb1be222e
Pugalendi A <39829202+PugalendiArumugam@users.noreply.github.com>
2025-07-23 23:47:31 +0530

    working on Testing E2E'

--- src/main/java/com/javaproject2/config/KafkaProducerConfig.java
+++ src/main/java/com/javaproject2/config/KafkaProducerConfig.java
@@ -1,14 +1,14 @@
-package com.javaproject2.config;
-
-import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class KafkaProducerConfig {
-
-    @Bean
-    public NewTopic createTopic(){
-        return new NewTopic("kafka-json-topics2",3,(short)1);
-    }
-}
+//package com.javaproject2.config;
+//
+//import org.apache.kafka.clients.admin.NewTopic;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class KafkaProducerConfig {
+//
+//    @Bean
+//    public NewTopic createTopic(){
+//        return new NewTopic("kafka-json-topics2",3,(short)1);
+//    }
+//}
--- src/test/java/com/javaproject2/KafkaProducerExampleApplicationTests.java
+++ src/test/java/com/javaproject2/KafkaProducerExampleApplicationTests.java
@@ -6,8 +6,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class KafkaProducerExampleApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
